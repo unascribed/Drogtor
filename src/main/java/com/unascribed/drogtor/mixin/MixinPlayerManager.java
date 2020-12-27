3aa4bd1c92c5c80620a78869672b0b69a92de1b4
Una Thompson <una@unascribed.com>
2020-12-27 15:56:03 -0800

    Force console join/leave messages to use real username
    Probably fixes #4

---
+++ src/main/java/com/unascribed/drogtor/mixin/MixinPlayerManager.java
@@ -0,0 +1,39 @@
+package com.unascribed.drogtor.mixin;
+
+import java.util.UUID;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.MessageType;
+import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+
+@Mixin(PlayerManager.class)
+public class MixinPlayerManager {
+
+	@Redirect(at=@At(value="INVOKE", target="net/minecraft/server/PlayerManager.broadcastChatMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V"),
+			method="onPlayerConnect(Lnet/minecraft/network/ClientConnection;Lnet/minecraft/server/network/ServerPlayerEntity;)V")
+	public void broadcastChatMessage(PlayerManager subject, Text msg, MessageType type, UUID senderUuid, ClientConnection conn, ServerPlayerEntity player) {
+		if (msg instanceof TranslatableText) {
+			TranslatableText tt = ((TranslatableText)msg);
+			String key = tt.getKey();
+			if ("multiplayer.player.joined".equals(key)) {
+				subject.sendToAll(new GameMessageS2CPacket(msg, type, senderUuid));
+				subject.getServer().sendSystemMessage(new TranslatableText("multiplayer.player.joined", player.getGameProfile().getName()), senderUuid);
+				return;
+			} else if ("multiplayer.player.joined".equals(key)) {
+				subject.sendToAll(new GameMessageS2CPacket(msg, type, senderUuid));
+				subject.getServer().sendSystemMessage(new TranslatableText("multiplayer.player.joined.renamed", player.getGameProfile().getName(), tt.getArgs()[1]), senderUuid);
+				return;
+			}
+		}
+		subject.broadcastChatMessage(msg, type, senderUuid);
+	}
+	
+}
---
+++ src/main/java/com/unascribed/drogtor/mixin/MixinServerPlayNetworkHandler.java
@@ -0,0 +1,39 @@
+package com.unascribed.drogtor.mixin;
+
+import java.util.UUID;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.network.MessageType;
+import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public class MixinServerPlayNetworkHandler {
+
+	@Shadow
+	public ServerPlayerEntity player;
+	
+	@Redirect(at=@At(value="INVOKE", target="net/minecraft/server/PlayerManager.broadcastChatMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V"),
+			method="onDisconnected(Lnet/minecraft/text/Text;)V")
+	public void broadcastChatMessage(PlayerManager subject, Text msg, MessageType type, UUID senderUuid) {
+		if (msg instanceof TranslatableText) {
+			TranslatableText tt = ((TranslatableText)msg);
+			String key = tt.getKey();
+			if ("multiplayer.player.left".equals(key)) {
+				subject.sendToAll(new GameMessageS2CPacket(msg, type, senderUuid));
+				subject.getServer().sendSystemMessage(new TranslatableText("multiplayer.player.left", player.getGameProfile().getName()), senderUuid);
+				return;
+			}
+		}
+		subject.broadcastChatMessage(msg, type, senderUuid);
+	}
+	
+}
